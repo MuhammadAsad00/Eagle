da737fcd853893e1b7e452e3cbf2b4f9b72ad9d1
Muhammad Asad <113167175+MuhammadAsad00@users.noreply.github.com>
2025-11-21 20:07:54 +0500

    Configure CORS with specific origin and credentials

--- backend/api/index.js
+++ backend/api/index.js
@@ -17,7 +17,10 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors(
+    origin: ["https://eagle-front-alpha.vercel.app/"],
+    credentials: true,
+));
 
 // Routes
 app.use("/api/auth", authRoute);
